0bb3dc56afdce1151b88e1da9d73dad9f3ec9700
KacperGryczon <134103128+KacperGryczon@users.noreply.github.com>
2025-09-16 19:39:59 +0200

    Delete kawiarnia directory

--- kawiarnia/główna.js
+++
@@ -1,9 +0,0 @@
-const navBar = document.getElementsByClassName("navigation");
-
-function openSidebar(){
-    navBar[0].classList.add('show');
-};
-
-function closeSidebar(){
-    navBar[0].classList.remove('show');
-};
